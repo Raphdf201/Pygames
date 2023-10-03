aa3ad3526bfe6065a71ecd4870ef0186000c3219
Raphdf201 <raphdf201@hotmail.com>
2023-10-03 12:31:23 -0400

    Separated the player and game classes in seperate files

--- Shooter/main.py
+++ Shooter/main.py
@@ -1,15 +1,18 @@
 import pygame
+
 pygame.init()
 
 # Fenêtre
 pygame.display.set_caption("Shooter")
-screen = pygame.display.set_mode((1600, 900))
+screen = pygame.display.set_mode((1080, 720))
 
 background = pygame.image.load('assets/bg.jpg')
+game = Game()
 
 running = True
 while running:
-    screen.blit(background, (0, 0))
+    screen.blit(background, (0, -200))
+    screen.blit(game.player.sprite, game.player.rect)
     pygame.display.flip()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
---
+++ Shooter/player.py
@@ -0,0 +1,13 @@
+import pygame
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.health = 100
+        self.health_max = 100
+        self.damage = 10
+        self.velocity = 5
+        self.sprite = pygame.image.load('Assets/player.png')
+        self.rect = self.sprite.get_rect()
+        self.rect.x = 450
+        self.rect.y = 500
